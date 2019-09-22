f69a4ff538b0a4c0ba9a7a388241fc74e7e724cd
n0d3tr <n0d3tr@gmail.com>
2019-09-22 08:52:38 -0500

    nueva landing

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,94 +3,32 @@ import React, { Component } from 'react'
 export default class ContentHome extends Component {
     render() {
       return (
-        <div id="colorlib-page">
-  <header>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="colorlib-navbar-brand">
-            <a className="colorlib-logo" href="/">
-              KodiakDesing
-            </a>
-          </div>
-          <a href="#a" className="js-colorlib-nav-toggle colorlib-nav-toggle">
-            <i />
-          </a>
-        </div>
-      </div>
-    </div>
-  </header>
-  <div id="colorlib-hero" className="js-fullheight">
-    <div className="owl-carousel">
-      <div className="item">
-        <div className="hero-flex js-fullheight">
-          <div className="col-three-forth">
-            <div
-              className="hero-img js-fullheight"
-              style={{ backgroundImage: "url(images/slider_home/2651929.png)" }}
-            />
-          </div>
-          <div className="col-one-forth js-fullheight">
-            <div className="display-t js-fullheight">
-              <div className="display-tc js-fullheight">
-                {/* <h2 class="number">01/03</h2> */}
-                <div className="text-inner">
-                  <div className="desc">
-                    <span className="tag">Bienvenido</span>
-                    <h2>DESARROLLO WEB.</h2>
-                    <p>
-                    Concepto claro + Diseño atractivo + Desarrollo preciso = ¡Página web asombrosa! Para esto, nos apoyamos en metodologías ágiles como Kanban, Scrum, conversión en mente, y otros.
-                    </p>
-                    <p>
-                      <a href="/Portfolio" className="btn-view">
-                        Ver portafolio <i className="icon-arrow-right3" />
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
+        <div>
+  <style dangerouslySetInnerHTML={{__html: " \n\t.fullscreen-bg {\n\tposition: fixed;\n\ttop: 0;\n\tright: 0;\n\tbottom: 0;\n\tleft: 0;\n\toverflow: hidden;\n\tz-index: -100;\n\t}\n\n\t.fullscreen-bg__video {\n\tposition: absolute;\n\ttop: 0;\n\tleft: 0;\n\twidth: 100%;\n\theight: 100%;\n\t}\n\n\t@media (min-aspect-ratio: 16/9) {\n\t.fullscreen-bg__video {\n\t\theight: 300%;\n\t\ttop: -100%;\n\t}\n\t}\n\n\t@media (max-aspect-ratio: 16/9) {\n\t.fullscreen-bg__video {\n\t\twidth: 300%;\n\t\tleft: -100%;\n\t}\n\t}\n\n\t@media (max-width: 767px) {\n\t/* .fullscreen-bg {\n\t\tbackground: url('./images/about.jpg') center center / cover no-repeat;\n\t} */\n\n\t.fullscreen-bg__video {\n\t\t/* display: none; */\n\t}\n\t}\n\t" }} />
+  <div className="fullscreen-bg">
+    <video loop muted autoPlay poster="images/about.jpg" className="fullscreen-bg__video">
+      <source src="particles.mp4" type="video/mp4" />
+    </video>
+  </div>
+  <div id="colorlib-page">
+    <header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="colorlib-navbar-brand">
+              {/* <a class="colorlib-logo" href="index.html">KodiakDesing</a> */}
             </div>
+            <a href="#" className="js-colorlib-nav-toggle colorlib-nav-toggle"><i style={{color: '#ffffff !important'}} /></a>
           </div>
         </div>
       </div>
+    </header>
+    <div id="colorlib-hero" className="js-fullheight">
+      {/* <div class="owl-carousel"> */}
       <div className="item">
         <div className="hero-flex js-fullheight">
           <div className="col-three-forth">
-            <div
-              className="hero-img js-fullheight"
-              style={{ backgroundImage: "url(images/slider_home/340668-PA9Q77-236.png)" }}
-            />
-          </div>
-          <div className="col-one-forth js-fullheight">
-            <div className="display-t js-fullheight">
-              <div className="display-tc js-fullheight">
-                {/* <h2 class="number">02/03</h2> */}
-                <div className="text-inner">
-                  <div className="desc">
-                    {/* <span class="tag">Photography</span> */}
-                    <h2>DESARROLLO DE APLICACIONES.</h2>
-                    <p>
-                    Más de 15.900 horas de desarrollo...y contando. iOS, Android, API’s, cloud backend… Nómbralo, y ¡lo hacemos!
-                    </p>
-                    <p>
-                      <a href="/Portfolio" className="btn-view">
-                        Ver portafolio <i className="icon-arrow-right3" />
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="item">
-        <div className="hero-flex js-fullheight">
-          <div className="col-three-forth">
-            <div
-              className="hero-img js-fullheight"
-              style={{ backgroundImage: "url(images/slider_home/2849327.png)" }}
-            />
+            <div className="hero-img js-fullheight" style={{backgroundImage: 'url(images/img_bg_3.4jpg)'}} />
           </div>
           <div className="col-one-forth js-fullheight">
             {/* <h2 class="number">03/03</h2> */}
@@ -98,16 +36,10 @@ export default class ContentHome extends Component {
               <div className="display-tc js-fullheight">
                 <div className="text-inner">
                   <div className="desc">
-                    <span className="tag">Descubrir</span>
-                    <h2>Posicionamiento en Google + Redes</h2>
-                    <p>
-                    Convertimos a tu Empresa en un referente del segmento pagando solo por clic o resultado.
-                    </p>
-                    <p>
-                      <a href="/Portfolio" className="btn-view">
-                        Ver portafolio <i className="icon-arrow-right3" />
-                      </a>
-                    </p>
+                    {/* <span class="tag">Descubrir</span> */}
+                    {/* <h2>Descubre nuevas opciones</h2> */}
+                    <img src="./images/logo.png" alt="" width="300px" /><br /><br />
+                    <p><a href="work.html" className="btn-view">Ver portafolio <i className="icon-arrow-right3" /></a></p>
                   </div>
                 </div>
               </div>
@@ -115,9 +47,11 @@ export default class ContentHome extends Component {
           </div>
         </div>
       </div>
+      {/* </div>
+		</div> */}
     </div>
-  </div>
-</div>
+  </div></div>
+
 
         )
     }
